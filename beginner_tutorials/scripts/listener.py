22ebfe401b794eb526ba51286e18a9706720f3cb
NavegacionHARLAN <50377548+NavegacionHARLAN@users.noreply.github.com>
2019-05-16 17:40:52 -0500

    Update listener.py

--- beginner_tutorials/scripts/listener.py
+++ beginner_tutorials/scripts/listener.py
@@ -33,14 +33,16 @@
 #
 # Revision $Id$
 
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
-
 import rospy
 import cv2
 import time
 import numpy as np
+from tf.transformations import euler_from_quaternion
+from tf2_msgs.msg import TFMessage
 from std_msgs.msg import String
+import serial
+import serial.tools.list_ports
+import math
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 client = ModbusClient(method = 'rtu', port='/dev/ttyUSB0', timeout=1, stopbits=1, bytsize=8, parity='N', baudrate=9600)
 client.connect()
@@ -52,73 +54,84 @@ cx1=0
 cx2=0
 cxf=0
 oops=0
-#def talker(a):
- #   pub = rospy.Publisher('chatter', String, queue_size=10)
-  #  rospy.init_node('talker', anonymous=True)
-   # rate = rospy.Rate(50) # 10hz
-    #while not rospy.is_shutdown():
-   # rospy.loginfo(a)
-   # pub.publish(a)
-   # rate.sleep()
+move=0
+fase=0
+contador=0
+inicial=0
+
+def callback(msg):
+	global fase
+	
+	q = msg.transforms[0].transform.rotation		#Quaterniones
+	
+	orientation_list = (q.x, q.y, q.z, q.w)
+	(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+	t = yaw
+	t = np.degrees(t)
+	print("yaw: %.2f" % t)
+	if(fase<10):	
+		fase=runo(t,fase)
+		print(fase)
+	elif(fase>10):
+		print("He acabado")
+
+def listener_lidar():
+    rospy.init_node('listener_lidar', anonymous=True)
+    rospy.Subscriber('/tf', TFMessage, callback)
+    rospy.spin()
+
 def avanzar():
 	#freno
 	print("VROOM")
-	client.write_register(44,826,unit=5) #valor inferior
-	client.write_register(45,827,unit=5) #medio
-	client.write_register(46,829,unit=5) #superior
+	client.write_register(44,587,unit=5) #valor inferior
+	client.write_register(45,591,unit=5) #medio
+	client.write_register(46,595,unit=5) #superior
 	#acelerador
 	client.write_register(16,8,unit=5) #valor inferior
 	client.write_register(17,14,unit=5) #superior
 	client.write_register(18,11,unit=5) #medio
 def alto():
 	#freno
-	print("PARA")
-	client.write_register(44,817,unit=5) #inferior
-	client.write_register(45,818,unit=5) #medio
-	client.write_register(46,822,unit=5) #superior
+	#print("PARA")
+	client.write_register(44,518,unit=5) #inferior
+	client.write_register(45,521,unit=5) #medio
+	client.write_register(46,526,unit=5) #superior
 	#acelerador
 	client.write_register(16,3,unit=5) #inferior
 	client.write_register(17,7,unit=5) #superior
 	client.write_register(18,5,unit=5) #medio
 def derecha():
-	print("derecha")
-	client.write_register(15,39,unit=5)#38
-def tower():
-	print("por un segundo")
-	client.write_register(15,28,unit=5)
+	client.write_register(15,24,unit=5)#38
 def izquierda():
-	print("izquierda")
-	client.write_register(15,25,unit=5)#22
+	client.write_register(15,16,unit=5)#22
 def frente():
-	print("frente")
-	client.write_register(15,29,unit=5)
+	client.write_register(15,20,unit=5)
 def obstaculo(espera,nmms):
-	#81 paro de emergencia
 	while(espera==1 or nmms==1):
 		rr=client.read_input_registers(83,1,unit=5)
 		espera=rr.registers[0]
 		rocky2=client.read_input_registers(81,1,unit=5)		
 		nmms=rocky2.registers[0]
 		#freno
-		client.write_register(44,817,unit=5) #inferior
-		client.write_register(45,818,unit=5) #medio
-		client.write_register(46,822,unit=5) #superior
+		client.write_register(44,518,unit=5) #inferior
+		client.write_register(45,521,unit=5) #medio
+		client.write_register(46,526,unit=5) #superior
 		#acelerador
 		client.write_register(16,3,unit=5)
-		client.write_register(17,7,unit=5)
+		client.write_register(17,6,unit=5)
 		client.write_register(18,5,unit=5)
 		time.sleep(1)
 def neutro():
 	#Freno
-	print("avanzo")
-	client.write_register(44,826,unit=5) #infrior+2
-	client.write_register(45,827,unit=5) #mdio
-	client.write_register(46,829,unit=5) #superior
+	client.write_register(44,587,unit=5) #valor inferior
+	client.write_register(45,591,unit=5) #medio
+	client.write_register(46,595,unit=5) #superior
 	#acelerador
 	client.write_register(16,3,unit=5) #inferior
 	client.write_register(17,6,unit=5) #superior
 	client.write_register(18,5,unit=5) #medio
 def parking():
+	print("parking")
 	client.write_register(48,1,unit=5)
 	time.sleep(0.2)
 	for y in range(5):
@@ -126,6 +139,7 @@ def parking():
 	client.write_register(48,0,unit=5)
 	time.sleep(3)
 def drive():
+	print("drive")
 	client.write_register(50,1,unit=5)
 	time.sleep(0.2)
 	for y in range(5):
@@ -133,82 +147,148 @@ def drive():
 	client.write_register(50,0,unit=5)
 	time.sleep(5)
 def reversa():
+	print("rvrsa")
 	client.write_register(49,1,unit=5)
 	time.sleep(0.2)
 	for y in range(5):
 		print("NOTETRABES")
 	client.write_register(49,0,unit=5)
 	time.sleep(3)
-def encendido():
+#def encendido():
 	#Arrancado de vehiculo			
 	#cambio velocidad			
-	print("parking")
-	client.write_register(48,1,unit=5)
-	time.sleep(0.2)
-	for i in range(4):
-		print("NOTETRABES")
-	client.write_register(48,0,unit=5)
-	time.sleep(1)
-	for x in range(5):
-		print("freno")
+#	print("parking")
+#	client.write_register(48,1,unit=5)
+#	time.sleep(0.2)
+#	for i in range(4):
+#		print("NOTETRABES")
+#	client.write_register(48,0,unit=5)
+#	time.sleep(1)
+#	for x in range(5):
+#		print("freno")
 		#freno
-		client.write_register(44,817,unit=5)
-		client.write_register(45,818,unit=5)
-		client.write_register(46,821,unit=5)
-		#direccion
-		#client.write_register(15,42,unit=5)
+#		client.write_register(44,518,unit=5) #inferior
+#		client.write_register(45,521,unit=5) #medio
+#		client.write_register(46,526,unit=5) #superior
 		#acelerador
-		client.write_register(16,5,unit=5)
-		client.write_register(17,7,unit=5)
-		client.write_register(18,6,unit=5)
-		time.sleep(1)
-	print("ignition")
-	client.write_register(84,0,unit=5)
-	time.sleep(0.2)
-	client.write_register(80,1,unit=5)
-def apagado():
-	client.write_register(80,0,unit=5) 
-	time.sleep(0.2)
-	client.write_register(84,1,unit=5)
+#		client.write_register(16,5,unit=5)
+#		client.write_register(17,7,unit=5)
+#		client.write_register(18,6,unit=5)
+#		time.sleep(1)
+#	print("ignition")
+#	client.write_register(84,0,unit=5)
+#	time.sleep(0.2)
+#	client.write_register(80,1,unit=5)
+#def apagado():
+#	client.write_register(80,0,unit=5) 
+#	time.sleep(0.2)
+#	client.write_register(84,1,unit=5)
 def cuidado():
 	global ohuh
 	global apagalootto
-	rocky6=client.read_input_registers(81,1,unit=5)
-	raios=client.read_input_registers(83,1,unit=5)
-	ohuh=raios.registers[0]
+	rocky6=client.read_input_registers(81,4,unit=5)
+	ohuh=rocky6.registers[2]
 	apagalootto=rocky6.registers[0]
 	if(ohuh==0 and apagalootto==0):	
-		print("vas bien")
 		return 0		
-	elif(ohuh==1 or apagalootto==1):
-		print("GOLPE AVISA")
-		obstaculo(ohuh,apagalootto)
+	elif(ohuh==1 and apagalootto==0):
+		return 1
+	elif(apagalootto==1):
+		return 2
 
-def runo():
-	tower()	
-	alto()
-	time.sleep(6)	
-	drive()
-	neutro()
-	tower()
-	time.sleep(2)
-	frente()
-	time.sleep(24)
-	izquierda()
-	time.sleep(11)
-	frente()
-	time.sleep(15)
-	#derecha()
-	#time.sleep(12)
-	#frente()
-	#time.sleep(4)
-	alto()
-	time.sleep(10)				
-
+def runo(t, fase):
+	global contador	
+	if(fase==0):
+		if(t>-72):			
+			derecha()
+			return 0
+		else:
+			frente()
+			return 1
+	elif(fase==1):	
+		contador=cuidado()
+		if(contador==1):
+			#neutro()			
+			return 2
+		elif(contador==2):
+			alto()			
+			return 6
+		else:
+			return 1
+	elif(fase==6):
+		contador=cuidado()
+		if(contador==2):
+			return 6
+		else:
+			neutro()			
+			return 1
+	elif(fase==2):
+		if(t<-25):
+			izquierda()
+			return 2
+		else:
+			frente()
+			#neutro()
+			time.sleep(0.6)		
+			alto()
+			time.sleep(4)
+			reversa()
+			time.sleep(2)
+			neutro()
+			#frente()
+			time.sleep(2)
+			return 3
+	elif(fase==3):
+		#reversa()
+		avanzar()
+		time.sleep(1)
+		for y in range(18):
+			derecha()
+			neutro()
+			contador=cuidado()
+			while(contador==2):
+				alto()
+				contador=cuidado()
+			time.sleep(0.5)
+		alto()
+		time.sleep(4)
+		drive()
+		time.sleep(4)
+		while(contador!=2):
+			frente()
+			neutro()
+			contador=cuidado()
+			while(contador==1):
+				alto()
+				contador=cuidado()
+				time.sleep(2)
+		alto()
+		time.sleep(6)
+		parking()
+		time.sleep(6)
+		client.write_register(36,0,unit=5)
+		return 11
+	elif(fase==7):
+		contador=cuidado()
+		if(contador==2):
+			return 7
+		else:
+			neutro()			
+			return 3
+	
 
 if __name__ == '__main__':
-    #while not rospy.is_shutdown():
-	client.write_register(36,1,unit=5)        
-	runo()
-    #except rospy.ROSInterruptException:
-     #   pass
+	move=0
+	client.write_register(36,1,unit=5)
+	frente()
+	alto()
+	time.sleep(4)
+	print("Presione el boton verde para continuar")
+	while(move==0):
+		rambo=client.read_input_registers(82,1,unit=5)
+		move=rambo.registers[0]
+	drive()
+	time.sleep(4)
+	neutro()
+	time.sleep(2)    
+	listener_lidar()
