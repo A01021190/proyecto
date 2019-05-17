95081172664e03aca673f99c25e02a7aaf515a36
NavegacionHARLAN <50377548+NavegacionHARLAN@users.noreply.github.com>
2019-05-16 19:20:07 -0500

    Update talker.py

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -33,13 +33,12 @@
 #
 # Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
+## Reemplaza talker.py en beginner_tutorials/scripts
+#Importacion de librerias importantes
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import rospy
 from std_msgs.msg import String
 import time
-#from std_msgs.msg import Empty
 import numpy as np
 import serial
 import serial.tools.list_ports
@@ -52,12 +51,14 @@ if os.name == 'nt':
 else:
   import tty, termios
 
-
+#reemplazar port='/dev/tty' por el puerto al que este conectado el PLC
 client = ModbusClient(method = 'rtu', port='/dev/ttyUSB0', timeout=1, stopbits=1, bytsize=8, parity='N', baudrate=9600)
+#En caso de no estar conectado al PLC, el programa arrojara un error en esta linea debido a que es la encargada de la comunicacion
 client.connect()
 count=0
 check=0
 t=0
+#Un mensaje que se puede editar 
 msg = """
 Control Your Harlan 2.0!
 ---------------------------
@@ -70,11 +71,13 @@ space key, s : force stop
 
 CTRL-C to quit
 """
-
+#En caso de un error
 e = """
 Communications Failed
 """
+
 def getKey(): #Input de teclado
+#es un metodo ya existente de ros
     if os.name == 'nt':
       return msvcrt.getch()
 
@@ -88,58 +91,37 @@ def getKey(): #Input de teclado
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-#def callback(msg):
-#	x = msg.transforms[0].transform.translation.x
-#	y = msg.transforms[0].transform.translation.y
-#	q = msg.transforms[0].transform.rotation		#Quaterniones
-	
-#	orientation_list = (q.x, q.y, q.z, q.w)
-#	(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-#	t = yaw
-#	t = np.degrees(t)
-	
-	#rospy.loginfo("X = %f :: Y = %f :: Teta = %.2f",x,y,t)
-#	prueba(t)
-
 def talker(a):
     #Nodo talker para ros
     pub = rospy.Publisher('chatter', String, queue_size=10)
+    #inicializa el nodo
     rospy.init_node('talker', anonymous=True)
+    #el rate en Hertz de cada cuanto publica al nodo
     rate = rospy.Rate(10) 
+    #el valor que se muestra mando
     rospy.loginfo(a)
+    #el valor a publicar en nodo
     pub.publish(a)
+    #duerme al nodo de acuerdo al rate
     rate.sleep()
-
-#def listener_lidar():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    #rospy.init_node('listener_lidar', anonymous=True)
-    #rospy.Subscriber('/tf', TFMessage, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    #rospy.spin()
-	
-	
+#Programa principal en un loop constante	
 def prueba(): 
 	global check
 	global count
-	f = open('/home/xhunter70/LOL.txt','w')
 	while(1):
 		if(check==0):
+			#imprime el mensaje la primera vez que corre
 			print msg
 		elif check > 10:
 			check=0
+		#se llama al metodo que tiene el input de teclado
 		key = getKey()
-		if key == 'w' :
+		if key == 'w' : #Avanzar presionando el pedal
 			check+=1
+			#publica un mensaje para retroalimentacion
 			talker("foward")
-			f.write("/foward")
-    			f.write(" \n")
 			#freno
+			#el método recibe primero el registro, luego el valor a escribir y finalmente la direccion del esclavo/PLC
 			client.write_register(44,826,unit=5) #valor inferior
 			client.write_register(45,827,unit=5) #medio
 			client.write_register(46,829,unit=5) #superior
@@ -147,19 +129,18 @@ def prueba():
 			client.write_register(16,8,unit=5) #valor inferior
 			client.write_register(17,14,unit=5) #superior
 			client.write_register(18,11,unit=5) #medio
+		#Girar a la izquierda
 		elif key == 'a' :
 			check+=1
 			talker("iz")
-			f.write("/iz")
-    			f.write(" \n")
-			#talker("2")
-			client.write_register(15,25,unit=5)
+			#en caso de la direccion solo es necesario un valor. se debe evitar manda el maximo permitdo
+			client.write_register(15,15,unit=5)
+		#Se pisa el freno y se centran las llantas
 		elif key == 's' :
 			check+=1
-			client.write_register(15,30,unit=5)
+			#direccion
+			client.write_register(15,20,unit=5)
 			talker("stop")
-			f.write("/stop")
-    			f.write(" \n")
 			#freno
 			client.write_register(44,817,unit=5) #inferior
 			client.write_register(45,818,unit=5) #medio
@@ -168,21 +149,17 @@ def prueba():
 			client.write_register(16,3,unit=5) #inferior
 			client.write_register(17,7,unit=5) #superior
 			client.write_register(18,5,unit=5) #medio
+		#girar a la derecha
 		elif key == 'd' :
 			check+=1
-			#talker("3")
 			talker("der")
-			f.write("/der")
-    			f.write(" \n")
-			#print(datetime.datetime.now().time())
-			client.write_register(15,39,unit=5)
+			client.write_register(15,25,unit=5)
+		#mantener las llantas centradas y los pedales sueltos
 		elif key == 'n' :
-			talker("neutro")
-			f.write("neutro")
-    			f.write(" \n")			
+			talker("neutro")		
 			check+=1
 			#direccion
-			client.write_register(15,30,unit=5)
+			client.write_register(15,20,unit=5)
 			#Freno 
 			client.write_register(44,826,unit=5) #infrior
 			client.write_register(45,827,unit=5) #mdio
@@ -191,17 +168,20 @@ def prueba():
 			client.write_register(16,3,unit=5) #inferior
 			client.write_register(17,7,unit=5) #superior
 			client.write_register(18,5,unit=5) #medio
+		#Se apaga el motor en caso de haber sido encendido con ignition
 		elif key == 'm' :
 			print("off")
 			client.write_register(80,0,unit=5) 
 			time.sleep(0.2)
 			client.write_register(84,1,unit=5)
+		#Encendido del vehículo sin llave, ignition
 		elif key == 'i' :
 			#Arrancado de vehiculo			
 			#cambio velocidad			
 			print("parking")
 			client.write_register(48,1,unit=5)
 			time.sleep(0.2)
+			#es necesario un delay para evitar trabar el mecanismo de la palanca de velocidades
 			for i in range(4):
 				print("NOTETRABES")
 			client.write_register(48,0,unit=5)
@@ -213,7 +193,7 @@ def prueba():
 				client.write_register(45,818,unit=5)
 				client.write_register(46,820,unit=5)
 				#direccion
-				#client.write_register(15,43,unit=5)
+				client.write_register(15,20,unit=5)
 				#acelerador
 				client.write_register(16,5,unit=5)
 				client.write_register(17,7,unit=5)
@@ -223,6 +203,7 @@ def prueba():
 			client.write_register(84,0,unit=5)
 			time.sleep(0.2)
 			client.write_register(80,1,unit=5)
+		#Cambia a "drive"
 		elif key == 'o' :
 			#Cambiar velocidad			
 			print("drive")
@@ -231,36 +212,17 @@ def prueba():
 			for y in range(5):
 				print("NOTETRABES")
 			client.write_register(50,0,unit=5)
+		#Permite controlar la direccion y los pedales
 		elif key == 'q' :
 			#Modo autonomo en PLC			
 			print("aver")
 			client.write_register(36,1,unit=5)
-		elif key == 'u' :
-			#Freno emerg on			
-			print("ALV")
-			client.write_register(52,0,unit=5)
-			client.write_register(51,1,unit=5)
-			time.sleep(0.5)
-			for y in range(5):
-				print("NOTETRABES")
-			client.write_register(51,0,unit=5)
-		elif key == 'y' :
-			#Freno emerg off
-			print("phew")
-			client.write_register(51,0,unit=5)
-			client.write_register(52,1,unit=5)
-			time.sleep(0.5)
-			for y in range(5):
-				print("NOTETRABES")
-			clien.write_register(52,0,unit=5)
+		#Desactiva el control para manejo manual
 		elif key == 'e' :
 			#Matar modo autonomo
 			print("muere prro")
 			client.write_register(36,0,unit=5)
-		#elif key == 'x' :
-			#Ence
-			#print("auto")
-			#client.write_register(80,1,unit=5)
+		#Cambiar a "parking"
 		elif key == 'p' :
 			print("park")
 			client.write_register(48,1,unit=5)
@@ -268,6 +230,7 @@ def prueba():
 			for y in range(5):
 				print("NOTETRABES")
 			client.write_register(48,0,unit=5)
+		#Cambiar a "reversa"
 		elif key == 'r' :
 			print("rever")
 			client.write_register(49,1,unit=5)
@@ -275,12 +238,13 @@ def prueba():
 			for y in range(5):
 				print("NOTETRABES")
 			client.write_register(49,0,unit=5)
+		#se acaba el programa
 		else:
 		        if (key == '\x03'):
 		            	break
 		check+=1
 
-if __name__ == '__main__':	
+if __name__ == '__main__':#metodo main que arroja un exception en caso de un error en la comunicacion	
 	if os.name != 'nt':
 		try:
 			settings = termios.tcgetattr(sys.stdin)
